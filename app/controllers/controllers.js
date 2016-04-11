eb3d625117d4a76610890489d2b262f26bf0d9f6
Chandan Kumar <chandan.jhun@gmail.com>
2016-04-12 02:19:10 +0530

    First commit

---
+++ app/controllers/controllers.js
@@ -0,0 +1,16 @@
+myApp.controller('mainCtrl', function($scope){
+    $scope.customers = [
+        {
+            name: 'Chandan',
+            city: 'Pune'
+        },
+        {
+            name: 'Alok',
+            city: 'Karnal'
+        },
+        {
+            name: 'Ruby',
+            city: 'Chennai'
+        }
+    ];
+})
